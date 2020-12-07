0743160c81a63f58695e98279b3526048b6d4267
Zidni Mubarock <zidmubarock@gmail.com>
2020-12-07 10:38:19 +0700

    upserting cog

--- apps/http_api/test/config_enpoint_test.exs
+++ apps/http_api/test/config_enpoint_test.exs
@@ -15,4 +15,13 @@ defmodule HttpApi.ConfigEnpointTest do
     created_cog = ConfigSVC.find(fixture |> Map.fetch!(:name))
     assert cog_json == created_cog |> Core.Config.as_json |> Poison.encode!
   end
+
+  test "upserting /api/cog" do
+    fixture = Fixture.cog_string_valid
+    make_call(:post, "/api/cog", %{cog: fixture  |> Map.put(:value, "old@mail.com")})
+    make_call(:post, "/api/cog", %{cog: fixture  |> Map.put(:value, "new@mail.com")})
+    latest_cog = ConfigSVC.find(fixture |> Map.fetch!(:name))
+    assert latest_cog.value == "new@mail.com"
+    assert 2 == Core.Model.Config |> select([c], count(c.id)) |> Core.Repo.one
+  end
 end
