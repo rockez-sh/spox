5f6f9129a019585fc55f9220b23b8bf45fbc5b9d
Zidni Mubarock <zidmubarock@gmail.com>
2020-12-14 20:15:26 +0700

    saved schema

--- spox-ui/src/pages/SchemaPage.js
+++ spox-ui/src/pages/SchemaPage.js
@@ -17,15 +17,37 @@ import {
 import { useState } from 'react';
 
 export default function SchemaPage () {
-  const [state, setState] = useState({name: null, desc: null, schema_value: null})
+  const [state, setState] = useState({
+    saving: false,
+    saved: false,
+    persisted: false,
+    form_data: { name: null, desc: null, value: '{}'
+  }})
 
   function stateUpdater(attribute) {
-    return function(e){setState({...state, [attribute]: e.target.value})}
+    return function(e){
+      let form_data = {...state.form_data, [attribute]: e.target.value}
+      setState({...state, form_data})
+    }
   }
 
   function submit() {
-    console.log(state)
+    setState({...state, saving: true})
+    fetch('http://localhost:5001/api/sch', {
+      method: 'POST', // or 'PUT'
+      headers: {'Content-Type': 'application/json', },
+      body: JSON.stringify({sch: state.form_data}),
+    })
+    .then(response => {
+      response.json()
+      setState({...state, saving: false})
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      setState({...state, saving: false})
+    });
   }
+
   return (
     <Pane>
       <Pane paddingBottom={10} borderBottom="muted" marginBottom={20}>
@@ -39,13 +61,13 @@ export default function SchemaPage () {
             required={true}
             width="60%"
             marginRight={40}
-            value={state.name}
+            value={state.form_data.name}
             onChange={ stateUpdater('name') }
           />
         </Pane>
         <TextareaField
           label="Description"
-          value={state.desc}
+          value={state.form_data.desc}
           placeholder="A brief words what this config are for"
           onChange={ stateUpdater('desc') }
         />
@@ -54,20 +76,22 @@ export default function SchemaPage () {
         <Pane elevation={3} padding={10}>
           <JSONInput
             id          = 'a_unique_id'
-            placeholder = { JSON.parse(state.schema_value) }
+            placeholder = { JSON.parse(state.form_data.value) }
             locale      = { locale }
             height      = '550px'
             width       = '100%'
             theme       = 'light_mitsuketa_tribute'
             style       = { {body: {fontSize: "16px", backgroundColor: "#ffffff" }} }
             confirmGood = {false}
-            onBlur = { (v) => { stateUpdater('schema_value')( {target: {value: v.json}} ) }}
+            onBlur = { (v) => { stateUpdater('value')( {target: {value: v.json}} ) }}
           />
         </Pane>
       </Pane>
       <Pane padding={20} marginTop={20} textAlign="right" background="tint1">
         <Button is={Link} to="/" iconBefore={SmallCrossIcon} marginRight={15}>Cancel</Button>
-        <Button appearance="primary" onClick={submit} iconBefore={SavedIcon}>Save</Button>
+        <Button appearance="primary" onClick={submit} iconBefore={state.saving ? null : SavedIcon} isLoading={state.saving}>
+          {state.saving ? 'Saving ...' : 'Save'}
+        </Button>
       </Pane>
     </Pane>
   )
