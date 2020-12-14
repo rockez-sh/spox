1275c17454b385066db509c74661b7501704f837
Zidni Mubarock <zidmubarock@gmail.com>
2020-12-14 15:13:33 +0700

    passive schema page with json editor

--- spox-ui/src/pages/ConfigPage.js
+++ spox-ui/src/pages/ConfigPage.js
@@ -1,13 +1,45 @@
 import {
   Pane,
   Heading,
-  WrenchIcon
+  WrenchIcon,
+  TextInputField,
+  TextareaField,
+  Text
 } from 'evergreen-ui'
+
 
 export default function ConfigPage (argument) {
   return (
     <Pane>
-      <Heading size={700}><WrenchIcon/>  Config</Heading>
+      <Pane paddingBottom={10} borderBottom="muted" marginBottom={20}>
+        <Heading size={700}><WrenchIcon/>  Config</Heading>
+      </Pane>
+      <Pane borderBottom="muted" marginBottom={20}>
+        <Pane display="flex">
+          <TextInputField
+            label="Name"
+            placeholder="Must be uniq per namespace, only alphanumeric & underscore are allowed"
+            required={true}
+            width="60%"
+            marginRight={40}
+          />
+          <TextInputField
+            label="Namespace"
+            placeholder="namespace for your config"
+            required={true}
+            width="35%"
+          />
+        </Pane>
+        <TextareaField
+          label="Description"
+          placeholder="A brief words what this config are for"
+        />
+      </Pane>
+      <Pane>
+        <Pane display="flex">
+          <Text size={500} lineHeight="32px" marginRight={20}>Schema</Text>
+        </Pane>
+      </Pane>
     </Pane>
   )
 }
--- spox-ui/src/pages/SchemaPage.js
+++ spox-ui/src/pages/SchemaPage.js
@@ -1,13 +1,74 @@
 import {
   Pane,
   Heading,
-  DocumentIcon
-} from 'evergreen-ui'
+  DocumentIcon,
+  TextInputField,
+  TextareaField,
+  Button,
+  SavedIcon,
+  SmallCrossIcon
+} from 'evergreen-ui';
+import JSONInput from 'react-json-editor-ajrm';
+import locale    from 'react-json-editor-ajrm/locale/en';
+import {
+  Link
+} from "react-router-dom";
 
-export default function SchemaPage (argument) {
+import { useState } from 'react';
+
+export default function SchemaPage () {
+  const [state, setState] = useState({name: null, desc: null, schema_value: null})
+
+  function stateUpdater(attribute) {
+    return function(e){setState({...state, [attribute]: e.target.value})}
+  }
+
+  function submit() {
+    console.log(state)
+  }
   return (
     <Pane>
-      <Heading size={700}><DocumentIcon/>  Schema</Heading>
+      <Pane paddingBottom={10} borderBottom="muted" marginBottom={20}>
+        <Heading size={700}><DocumentIcon/>  Schema</Heading>
+      </Pane>
+      <Pane borderBottom="muted" marginBottom={20}>
+        <Pane display="flex">
+          <TextInputField
+            label="Name"
+            placeholder="Must be uniq per namespace, only alphanumeric & underscore are allowed"
+            required={true}
+            width="60%"
+            marginRight={40}
+            value={state.name}
+            onChange={ stateUpdater('name') }
+          />
+        </Pane>
+        <TextareaField
+          label="Description"
+          value={state.desc}
+          placeholder="A brief words what this config are for"
+          onChange={ stateUpdater('desc') }
+        />
+      </Pane>
+      <Pane>
+        <Pane elevation={3} padding={10}>
+          <JSONInput
+            id          = 'a_unique_id'
+            placeholder = { JSON.parse(state.schema_value) }
+            locale      = { locale }
+            height      = '550px'
+            width       = '100%'
+            theme       = 'light_mitsuketa_tribute'
+            style       = { {body: {fontSize: "16px", backgroundColor: "#ffffff" }} }
+            confirmGood = {false}
+            onBlur = { (v) => { stateUpdater('schema_value')( {target: {value: v.json}} ) }}
+          />
+        </Pane>
+      </Pane>
+      <Pane padding={20} marginTop={20} textAlign="right" background="tint1">
+        <Button is={Link} to="/" iconBefore={SmallCrossIcon} marginRight={15}>Cancel</Button>
+        <Button appearance="primary" onClick={submit} iconBefore={SavedIcon}>Save</Button>
+      </Pane>
     </Pane>
   )
 }
